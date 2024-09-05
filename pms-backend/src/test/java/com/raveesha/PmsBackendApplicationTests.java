5626271285efab4cf0f110c39f0c47d716ff2e8a
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-09-05 12:10:59 +0530

    Initial commit

---
+++ pms-backend/src/test/java/com/raveesha/PmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raveesha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
