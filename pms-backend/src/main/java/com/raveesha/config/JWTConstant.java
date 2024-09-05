abe205eb4d0a49b1d02a7ee8d3e2164a30b43221
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-09-06 00:39:05 +0530

    Configure spring security for JWT

---
+++ pms-backend/src/main/java/com/raveesha/config/JWTConstant.java
@@ -0,0 +1,6 @@
+package com.raveesha.config;
+
+public class JWTConstant {
+    public static final String SECRETE_KEY = "sdfsdfsdf ewrer dfdsfdfg";
+    public static final String JWT_HEADER = "Authorization";
+}
--- pms-backend/src/main/java/com/raveesha/config/OpenAPIConfiguration.java
+++ pms-backend/src/main/java/com/raveesha/config/OpenAPIConfiguration.java
@@ -1,2 +1,32 @@
-package com.raveesha.config;public class OpenAPIConfiguration {
+package com.raveesha.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+    
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8081");
+        server.setDescription("Development");
+        
+        Contact myContact = new Contact();
+        myContact.setName("Raveesha Induwara");
+        myContact.setEmail("raveeshe.abeysinghe@gmail.com");
+        
+        Info information = new Info()
+                                   .title("Project Management System API")
+                                   .version("1.0")
+                                   .description("This API exposes endpoints to manage project management system.")
+                                   .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
 }
